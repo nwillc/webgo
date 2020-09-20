78ffa906ccb22983f733e632a53a4f03d6271d1b
nwillc <nwillc@gmail.com>
2020-09-20 13:06:54 -0400

    Cleaning

--- main.go
+++ main.go
@@ -9,15 +9,14 @@ import (
 )
 
 func main() {
-	log.Printf("CONFIG_FOO set to: %s\n", os.Getenv("CONFIG_FOO"))
-	log.Printf("CONFIG_DATABASE_NAME set to: %s\n", os.Getenv("CONFIG_DATABASE_NAME"))
-	http.HandleFunc("/", handler)
+	msg := os.Getenv("CONFIG_MESSAGE")
 	go func() {
 		for {
 			time.Sleep(5 * time.Second)
-			log.Println("TICK")
+			log.Println(msg)
 		}
 	}()
+	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
