711e0cef213fd7fb94d5522422a35bf72810fba7
nwillc <nwillc@gmail.com>
2020-09-23 00:25:09 -0400

    only proxy within cluster

--- bin/istio-install.sh
+++ bin/istio-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-istioctl install --set profile=demo
+istioctl install --set profile=demo --set values.global.proxy.includeIPRanges="10.96.0.0/12"
 kubectl label namespace default istio-injection=enabled
 
