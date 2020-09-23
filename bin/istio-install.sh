f510e8ee69d99288960a54f8dbc450f2de2fecf0
nwillc <nwillc@gmail.com>
2020-09-23 01:24:38 -0400

    Working version, Istio allows direct access to google.com.

--- bin/istio-install.sh
+++ bin/istio-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Exclude the google class B from istio proxying.
 istioctl install --set profile=demo --set values.global.proxy.excludeIPRanges="172.217.0.0/16"
 kubectl label namespace default istio-injection=enabled
 
