8a68f4deaec4d910fa9023a71806c40b1e51d430
nchristopher <nchristopher@paxos.com>
2020-09-25 15:43:25 -0400

    - Add ServiceEntry for gobyexample.com - Add kiali & prometheus to istio

--- bin/istio-install.sh
+++ bin/istio-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-istioctl install --set profile=demo --set values.global.proxy.excludeIPRanges="172.217.0.0/16"
+istioctl install --set profile=demo --set values.global.proxy.excludeIPRanges="172.217.0.0/16\,40.83.210.182/32\,13.64.245.217/32"
 kubectl label namespace default istio-injection=enabled
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/kiali.yaml
 
