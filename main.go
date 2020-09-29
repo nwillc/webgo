84e2ecc94193099290a4c81fa7544e16845cc4c3
nwillc <nwillc@gmail.com>
2020-09-28 22:55:27 -0400

    Reading a file from a k9s volume

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 )
 
 const (
-	target1 = "http://gobyexample.com"
-	target2 = "http://google.com"
-	delay   = 10 * time.Second
+	target1       = "http://gobyexample.com"
+	target2       = "http://google.com"
+	delay         = 10 * time.Second
+	licenseFolder = "/license"
 )
 
 var msg string
@@ -25,6 +28,7 @@ func init() {
 
 func main() {
 	log.Println("Username:", os.Getenv("SECRET_USERNAME"))
+	licenseCheck(licenseFolder)
 	go pinger()
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8888", nil))
@@ -53,4 +57,26 @@ func pinger() {
 		}
 		time.Sleep(delay)
 	}
+}
+
+func licenseCheck(licenseFolder string) {
+	var files []string
+	err := filepath.Walk(licenseFolder, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		log.Println("Error walking", licenseFolder, err)
+		return
+	}
+	log.Println("Found")
+	for _, file := range files {
+		log.Println(file)
+	}
+	contents, err := ioutil.ReadFile(licenseFolder + "/license")
+	if err != nil {
+		log.Println("Could not open", licenseFolder, "/license")
+		return
+	}
+	log.Println("License:", string(contents))
 }
