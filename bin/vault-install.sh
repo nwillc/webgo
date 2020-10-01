b2f7a1f210b638baa044e741e40cbb0229e5f84c
nchristopher <nchristopher@paxos.com>
2020-10-01 17:23:57 -0400

    Cleaned vault install and chart to support vault

--- bin/vault-install.sh
+++ bin/vault-install.sh
@@ -3,31 +3,44 @@
 kubectx docker-desktop
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
-helm install vault hashicorp/vault --set "server.dev.enabled=true"
+helm install vault hashicorp/vault --atomic --set "server.dev.enabled=true"
 
-sleep 10
-kubectl exec -it vault-0 -- /bin/sh <<!
-# Enable kv-v2 secrets at the path internal
-vault secrets enable -path=internal kv-v2
-# Create a secret at path internal/database/config with a username and password
-vault kv put internal/database/config username="db-readonly-username" password="db-secret-password"
+echo
+echo -n "Waiting for vault-0 pod to be ready..."
+while [[ $(kubectl get pods vault-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' 2> /dev/null) != "True" ]]; do
+  sleep 2
+done
+echo done
+echo
+
+kubectl exec -i vault-0 -- /bin/sh <<!
+# Enable kv-v2 secrets at the path webgo
+vault secrets enable -path=webgo kv-v2
+
+# Create a secret at path webgo/database/config with a username and password
+vault kv put webgo/database/config username="db-readonly-username" password="db-secret-password"
+
 # Enable the Kubernetes authentication method.
 vault auth enable kubernetes
+
 # Configure the authentication to use the service account token, the location of the host, and its certificate.
 vault write auth/kubernetes/config \
-    token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+    token_reviewer_jwt=@/var/run/secrets/kubernetes.io/serviceaccount/token \
     kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
     kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-# Write out policy that enables the read capability for secrets at path internal/data/database/config.
-vault policy write webgo-app - <<EOF
-path "internal/data/database/config" {
+
+# Write out policy that enables the read capability for secrets at path webgo/data/database/config.
+vault policy write webgo - <<EOF
+path "webgo/data/database/config" {
   capabilities = ["read"]
 }
 EOF
+
 # Create a Kubernetes authentication role
-vault write auth/kubernetes/role/webgo-app \
-    bound_service_account_names=webgo-app \
+vault write auth/kubernetes/role/webgo \
+    bound_service_account_names=webgo \
     bound_service_account_namespaces=default \
-    policies=webgo-app \
+    policies=webgo \
     ttl=24h
+
 !
