605fb5e8fb47e4afc3971260c33aaf865a529d75
nwillc <nwillc@gmail.com>
2020-11-09 00:49:40 -0500

    Use helmfile

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,14 +3,14 @@ set -e
 set -u
 set -o pipefail
 
-DEBUG=""
-ENV=local
+COMMAND="sync"
 REPOSITORY="nwillc/webgo"
-VERSION="1.0.0"
+VERSION=""
 BUILD="false"
 CONTEXT=docker-desktop
+ENVIRONMENT=local
 
-while getopts ":bcdr:v:" OPT; do
+while getopts ":bcde:r:v:" OPT; do
   case "${OPT}" in
     b)
       BUILD="true"
@@ -19,7 +19,10 @@ while getopts ":bcdr:v:" OPT; do
       CONTEXT=${OPTARG}
       ;;
     d)
-      DEBUG="--dry-run --debug"
+      COMMAND="template"
+      ;;
+    e)
+      ENVIRONMENT=${OPTARG}
       ;;
     r)
       REPOSITORY=${OPTARG}
@@ -32,7 +35,8 @@ while getopts ":bcdr:v:" OPT; do
 script usage: $(basename $0) [-b] [-c context] [-d] [-r repository] [-v version]
   -b              Build executable.
   -c context      K8s context.
-  -d              Debug the helm charts w/o deploying.
+  -d              Debug the helm configuration w/o deploying.
+  -e environment  Environment configure charts for.
   -r repository   The image repository to deploy image to.
   -v version      The version number of the image.
 !
@@ -41,13 +45,17 @@ script usage: $(basename $0) [-b] [-c context] [-d] [-r repository] [-v version]
   esac
 done
 
+if [ -z "${VERSION}" ]; then
+  echo Version required.
+  exit 1
+fi
+
 if [ ".${BUILD}" == ".true" ]; then
   ./bin/docker-build-push.sh -r "${REPOSITORY}" -v "${VERSION}"
 fi
 
 kubectx "${CONTEXT}"
 
-helm upgrade --install \
-  --values environment/global/config.yaml --values "environment/${ENV}/config.yaml" \
-  --set image.repository="${REPOSITORY}" --set image.tag="${VERSION}" \
-  webgo ${DEBUG} ./charts/webgo
+echo helmfile -e ${ENVIRONMENT} -f charts/helmfile.yaml ${COMMAND} --set image.repository="${REPOSITORY}" --set image.tag="${VERSION}"
+helmfile -e ${ENVIRONMENT} -f charts/helmfile.yaml ${COMMAND} --set image.repository="${REPOSITORY}" --set image.tag="${VERSION}"
+
