ffa55985fddcd834f1847a724f728a9c3cdc9f83
nwillc <nwillc@gmail.com>
2020-11-17 00:05:31 -0500

    Istio sidecar experiments

--- bin/istio-install.sh
+++ bin/istio-install.sh
@@ -2,7 +2,7 @@
 
 echo Installing istio 1.7.4 demo profile, prometheus and kiali.
 istioctl install --set profile=demo
-kubectl label namespace default istio-injection=enabled
+kubectl label namespace default istio-injection=enable
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/prometheus.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/kiali.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/grafana.yaml
